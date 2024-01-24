cd4fab69e20d6ceaedca037ac1074ea88b0823d8
Sabarinath P <sabarinathp@Sabarinaths-Laptop.local>
2024-01-24 12:29:13 +0530

    fix -  all module fixed

--- lib/Screen/LoginScreen/LoginScreen.dart
+++ lib/Screen/LoginScreen/LoginScreen.dart
@@ -11,7 +11,6 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-<<<<<<< HEAD
             body: SingleChildScrollView(
       child: Column(
         children: [
@@ -20,17 +19,6 @@ class LoginScreen extends StatelessWidget {
             height: 33.6.h,
             alignment: Alignment.bottomCenter,
             child: Image.asset("assets/images/logo.png"),
-=======
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            Get.to(() => HomeScreen(), transition: Transition.rightToLeft);
-          },
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.orange,
->>>>>>> 2dae090 (changed)
           ),
           SizedBox(
             height: 8.h,
@@ -104,7 +92,7 @@ class LoginScreen extends StatelessWidget {
           Center(
             child: InkWell(
               onTap: () {
-                Get.to(() => homeScreen(), transition: Transition.rightToLeft);
+                Get.to(() => HomeScreen(), transition: Transition.rightToLeft);
               },
               child: Container(
                 width: 84.2.w,
