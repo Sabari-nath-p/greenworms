686796d726daf92e9d1000ddc2756d118c0fdd55
unknown <nandanasanal321@gmail.com>
2024-01-24 15:03:12 +0530

    status

--- lib/Screen/JobAssignedScreen/Component/StatusBar.dart
+++ lib/Screen/JobAssignedScreen/Component/StatusBar.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class StatusBar extends StatelessWidget {
   const StatusBar({super.key});
-
+ List se
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Row(
+     
+      children: [
+      
+        Align(
+          alignment: Alignment.topLeft,
+          child:Padding(padding: EdgeInsets.fromLTRB(5.w, 3.52.h, 0, 0),
+          child: Container(
+            width:92.05.w ,
+            height:5.8.h ,
+            decoration: BoxDecoration(border: Border.all(color: Color(0xff036163),width: 0.6.w,),borderRadius:BorderRadius.circular(0.9.h) ),
+          child: Row(
+            children: [
+             Padding(
+               padding:  EdgeInsets.only(left: 4.w),
+               child: Container(
+                width: 24.7.w,
+                height: 3.52.h,
+                alignment: Alignment.center,
+                child: Text("Assigned",style: GoogleFonts.lexend (fontSize: 10.sp,fontWeight: FontWeight.w500,color: Colors.white)),
+                decoration: BoxDecoration(color: Color(0xff036163),borderRadius:BorderRadius.circular(0.9.h))),
+             ),
+            
+             Padding(padding: EdgeInsets.only(left: 8.w),
+             child: Text("In Progress",style: GoogleFonts.lexend (fontSize: 10.sp,fontWeight: FontWeight.w500,color: Color(0xff036163))),
+               
+         ), Padding(padding: EdgeInsets.only(left: 8.w),
+             child: Text("Completed",style: GoogleFonts.lexend (fontSize: 10.sp,fontWeight: FontWeight.w500,color: Color(0xff036163))),
+               
+         ),
+
+
+            ], ),
+          
+          ),
+          )
+          
+        ),
+         
+      ],
+    );
   }
 }
+ 
--- lib/Screen/JobAssignedScreen/JobScreen.dart
+++ lib/Screen/JobAssignedScreen/JobScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:greenworms/Screen/JobAssignedScreen/Component/StatusBar.dart';
+import 'package:sizer/sizer.dart';
 
 class JobScreen extends StatelessWidget {
   const JobScreen({super.key});
@@ -10,7 +12,32 @@ class JobScreen extends StatelessWidget {
         child: Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [StatusBar(), Text("hello")],
+          children: [StatusBar(),
+          SizedBox(height: 2.8.h,),
+          Padding(
+            padding: EdgeInsets.only(left: 2.w),
+            child: SizedBox(
+                        width: 92.05.w,
+                        height: 5.8.h,
+                        child: TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search,size: 5.6.w,color: Color(0xff036163),),
+                            isDense: true,
+                            hintText: ("Search by Job Id"),
+                            hintStyle: GoogleFonts.lexend(color: Color(0xff036163),),
+                            
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(0.9.h),
+                              borderSide: BorderSide(
+                                  color: Color(0xffD0D5DD), width: 0.15.h),
+                            ),
+                          ),
+                        ),
+                      ),
+          ),
+          
+           ],
         ),
       ),
     ));
