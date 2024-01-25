d9f67aff4a2b64ba15c3252f1182284e091995bf
unknown <nandanasanal321@gmail.com>
2024-01-25 18:31:33 +0530

    password

--- lib/Screen/ProfileScreen/ChangePassword.dart
+++ lib/Screen/ProfileScreen/ChangePassword.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart';
 import 'package:sizer/sizer.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
@@ -16,7 +18,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
       child: Scaffold(
         body: SingleChildScrollView(
           child: Padding(
-            padding:EdgeInsets.only(left: 3.9.w),
+            padding:EdgeInsets.only(left: 5.9.w),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -29,7 +31,11 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                       height: 5.5.w,
                       
                       alignment: Alignment.topLeft,
-                      child: Icon(Icons.arrow_back_ios_new,size: 5.w,)),
+                      child: InkWell(
+                        onTap: () {
+                        Get.back();
+                        },
+                        child: Icon(Icons.arrow_back_ios_new,size: 5.w,))),
                         SizedBox(width: 2.w,),
                 Text("Change Password",style: GoogleFonts.lexend(fontSize:13.3.sp,fontWeight: FontWeight.w300,color: Colors.black ),)
             ,  
@@ -38,10 +44,10 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                 SizedBox(height: 7.6.h,),
                SizedBox(width:34.5.w ,
                height:2.35.h ,
-                child: Text("Change Password",style: GoogleFonts.lexend(fontSize:11.7.sp,fontWeight: FontWeight.w500,color:Color(0xff344054) ),))
+                child: Text("Current Password",style: GoogleFonts.lexend(fontSize:11.7.sp,fontWeight: FontWeight.w500,color:Color(0xff344054) ),))
             ,  SizedBox(height: 1.h,),
             SizedBox(
-               width: 84.2.w,
+               width: 88.2.w,
                         height: 5.17.h,
               child: TextFormField(
                            
@@ -65,7 +71,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                 child: Text("New Password",style: GoogleFonts.lexend(fontSize:11.7.sp,fontWeight: FontWeight.w500,color:Color(0xff344054) ),))
             ,  SizedBox(height: 1.h,),
             SizedBox(
-               width: 84.2.w,
+               width: 88.2.w,
                         height: 5.17.h,
               child: TextFormField(
                            
@@ -89,7 +95,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                 child: Text("Confirm Password",style: GoogleFonts.lexend(fontSize:11.7.sp,fontWeight: FontWeight.w500,color:Color(0xff344054) ),))
             ,  SizedBox(height: 1.h,),
             SizedBox(
-               width: 84.2.w,
+               width: 88.2.w,
                         height: 5.17.h,
               child: TextFormField(
                            
@@ -107,7 +113,23 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                             ),
                           ),
             ),
-          
+              SizedBox(height: 40.h,),
+                  Container(
+                  width: 88.2.w,
+                  height: 5.17.h,
+                  alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                      color: Color(0xff036163),
+                      borderRadius: BorderRadius.circular(0.9.h)),
+                  child: Text("Save",
+                      style: GoogleFonts.lexend(
+                        textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13.sp,
+                            fontWeight: FontWeight.w500),
+                      )),
+                ),
+                SizedBox(height: 2.h,),
               ],
             ),
           ),
