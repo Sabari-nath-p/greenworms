48652f7bda424abab90cfb14d02fa9194854f304
ReemaShareen <99016463+Reemasalam@users.noreply.github.com>
2024-01-25 23:37:53 +0530

    fix

--- lib/Screen/weightScreen/weightScreen.dart
+++ lib/Screen/weightScreen/weightScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sizer/sizer.dart';
 
 class weightScreen extends StatelessWidget {
@@ -10,109 +10,126 @@ class weightScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+         
+            
             Container(
-              width: 6.57.w,
-              height: 2.94.h,
-              margin: EdgeInsets.only(
-                top: 8.5.h,
-                left: 5.2.w,
+              width: 6.57.w, 
+              height: 2.94.h, 
+             margin: EdgeInsets.only(
+                top: 8.5.h, 
+                 left: 5.2.w, 
               ),
-              child: InkWell(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: Image.asset('assets/image/angle.png')),
+              child: Image.asset('assets/image/angle.png'),
             ),
-            SizedBox(
-              height: 2.h,
-            ),
+            SizedBox(height: 2.h,),
             Container(
               width: 32.w,
-              height: 3.5.h,
+              height:3.5.h,
+
               margin: EdgeInsets.only(
+                
+               
                 left: 7.4.w,
               ),
               child: Text(
                 'Enter Weight',
                 style: TextStyle(
-                    fontFamily: 'Lexend',
-                    fontSize: 16.66.sp,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: -0.01.w),
+                  fontFamily: 'Lexend',
+                  fontSize: 16.66.sp,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                
+                  letterSpacing: -0.01.w
+                  
+                ),
               ),
             ),
             SizedBox(height: 1.5.h),
             Container(
               width: 81.31.w,
-              height: 2.5.h,
+              height:2.5.h,
+
               margin: EdgeInsets.only(
+                
+               
                 left: 7.4.w,
               ),
               child: Text(
                 'തൂക്കം അളന്നതിനു ശേഷം ഇവിടെ രേഖപ്പെടുത്തുക ',
                 style: TextStyle(
-                    fontFamily: 'Lexend',
-                    fontSize: 10.88.sp,
-                    color: Color.fromRGBO(73, 73, 73, 1),
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: -0.01.w),
+                  fontFamily: 'Lexend',
+                  fontSize: 10.88.sp,
+                  color: Color.fromRGBO(73, 73, 73, 1),
+
+                  fontWeight: FontWeight.w500,
+                
+                  letterSpacing: -0.01.w
+                  
+                ),
               ),
             ),
-            SizedBox(height: 6.82.h),
-            Row(
-              children: [
-                Container(
-                  width: 13.68.w,
-                  height: 0.24.h,
-                  margin: EdgeInsets.only(
-                    left: 8.68.w,
-                  ),
-                  color: Color.fromRGBO(3, 97, 99, 1),
-                ),
-                Container(
-                  width: 13.68.w,
-                  height: 0.24.h,
-                  margin: EdgeInsets.only(
-                    left: 8.68.w,
-                  ),
-                  color: Color.fromRGBO(3, 97, 99, 1),
-                ),
-                Container(
-                  width: 13.68.w,
-                  height: 0.24.h,
-                  margin: EdgeInsets.only(
-                    left: 8.68.w,
-                  ),
-                  color: Color.fromRGBO(3, 97, 99, 1),
-                ),
-                Container(
-                  width: 13.68.w,
-                  height: 0.24.h,
-                  margin: EdgeInsets.only(
-                    left: 8.68.w,
-                  ),
-                  color: Color.fromRGBO(3, 97, 99, 1),
-                ),
-              ],
+             SizedBox(height: 6.82.h),
+             Container(
+              width: 13.68.w,
+              height: 0.24.h,
+              margin: EdgeInsets.only(
+                
+                left: 8.68.w,
+              ),
+              color: Color.fromRGBO(3, 97, 99, 1),
+            ),
+             Container(
+              width: 13.68.w,
+              height: 0.24.h,
+              margin: EdgeInsets.only(
+                
+                left: 24.2.w,
+              ),
+              color: Color.fromRGBO(3, 97, 99, 1),
+            ),
+            Container(
+              width: 13.68.w,
+              height: 0.24.h,
+              margin: EdgeInsets.only(
+                
+                left: 39.73.w,
+              ),
+              color: Color.fromRGBO(3, 97, 99, 1),
+            ),
+            Container(
+              width: 13.68.w,
+              height: 0.24.h,
+              margin: EdgeInsets.only(
+                
+                left: 55.26.w,
+              ),
+              color: Color.fromRGBO(3, 97, 99, 1),
             ),
             SizedBox(height: 3.h),
+            
             Container(
               width: 23.68.w,
-              height: 2.35.h,
+              height:2.35.h,
+
               margin: EdgeInsets.only(
+                
+               
                 left: 7.4.w,
               ),
               child: Text(
                 ' Enter Weight',
                 style: TextStyle(
-                    fontFamily: 'Lexend',
-                    fontSize: 11.66.sp,
-                    color: Color.fromRGBO(52, 64, 84, 1),
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: -0.01.w),
+                  fontFamily: 'Lexend',
+                  fontSize: 11.66.sp,
+                  color: Color.fromRGBO(52, 64, 84, 1),
+
+                  fontWeight: FontWeight.w500,
+                
+                  letterSpacing: -0.01.w
+                  
+                ),
               ),
             ),
             SizedBox(height: 2.5.h),
@@ -120,19 +137,20 @@ class weightScreen extends StatelessWidget {
               width: 84.21.w,
               height: 6.h,
               margin: EdgeInsets.only(left: 7.12.w),
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.only(left: 3.4.w),
+              padding: EdgeInsets.fromLTRB(7.4.w, 1.64.h, 2.63.w, 1.64.h),
               decoration: BoxDecoration(
+              
                 borderRadius: BorderRadius.circular(2.66.w),
                 border: Border.all(color: Color.fromRGBO(208, 213, 221, 1)),
                 boxShadow: [
                   BoxShadow(
-                    color: Color.fromRGBO(0, 0, 0, 0),
+                    color: Color.fromRGBO(0,0,0,0),
                     blurRadius: .50.w,
                     offset: Offset(0, 1),
                   ),
                 ],
                 gradient: LinearGradient(
+                  
                   colors: [
                     Color.fromRGBO(255, 255, 255, 1),
                     Color.fromRGBO(255, 255, 255, 1),
@@ -140,9 +158,7 @@ class weightScreen extends StatelessWidget {
                 ),
               ),
               child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    border: InputBorder.none, hintText: "Enter Weight"),
+                
                 style: TextStyle(
                   fontFamily: 'Lexend',
                   fontSize: 11.66.sp,
@@ -150,38 +166,51 @@ class weightScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 3.h),
+             SizedBox(height: 3.h),
             Container(
               width: 55.52.w,
-              height: 3.h,
+              height:3.h,
+
               margin: EdgeInsets.only(
+                
+               
                 left: 7.4.w,
               ),
               child: Text(
                 'ഫോട്ടോ അപ്‌ലോഡ് ചെയ്യുക ',
                 style: TextStyle(
-                    fontFamily: 'Lexend',
-                    fontSize: 10.88.sp,
-                    color: Color.fromRGBO(73, 73, 73, 1),
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: -0.01.w),
+                  fontFamily: 'Lexend',
+                  fontSize: 10.88.sp,
+                  color: Color.fromRGBO(73, 73, 73, 1),
+
+                  fontWeight: FontWeight.w500,
+                
+                  letterSpacing: -0.01.w
+                  
+                ),
               ),
             ),
-            SizedBox(height: 2.58.h),
+            SizedBox(height:2.58.h),
             Container(
               width: 83.68.w,
-              height: 12.82.h,
+              height: 12.82.h, 
               margin: EdgeInsets.only(left: 7.4.w),
-              child: Image.asset('assets/image/upload.png'),
+              child: InkWell(child: Image.asset('assets/image/upload.png'),onTap:()async{
+                final ImagePicker picker = ImagePicker(); print("hello");
+
+                final XFile? photo = await picker.pickImage(source: ImageSource.camera);
+
+              }),
+              
             ),
             SizedBox(height: 28.17.h),
-            Container(
+                 Container(
               width: 84.21.w,
               height: 5.17.h,
               margin: EdgeInsets.only(left: 7.36.w),
               child: ElevatedButton(
                 onPressed: () {
-                  // Handle the button tap action
+                  
                 },
                 style: ElevatedButton.styleFrom(
                   primary: Color.fromRGBO(3, 97, 99, 1),
@@ -213,4 +242,4 @@ class weightScreen extends StatelessWidget {
       ),
     );
   }
-}
+} 
