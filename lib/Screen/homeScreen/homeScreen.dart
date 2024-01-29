ebe616d48e3dfbb0bd0af0708a486a34e65dd7cd
ReemaShareen <99016463+Reemasalam@users.noreply.github.com>
2024-01-30 01:44:07 +0530

    joblist

--- lib/Screen/homeScreen/homeScreen.dart
+++ lib/Screen/homeScreen/homeScreen.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:greenworms/Screen/homeScreen/components/HMTitle.dart';
 import 'package:greenworms/Screen/homeScreen/components/HMTsmall.dart';
 import 'package:greenworms/Screen/homeScreen/components/JobListCard.dart';
+import 'package:greenworms/Screen/homeScreen/controller.dart';
 import 'package:sizer/sizer.dart';
 
 import 'components/HMTcard.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+   HomeScreen({super.key});
+   homeController homeCtrl = Get.put(homeController());
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        // HPrimaryCard(),
-        Positioned(
-          child: Container(
-            width: 100.w,
-            height: 25.76.h,
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(2, 97, 99, 1),
+    return GetBuilder<homeController>(
+      builder: (context) {
+        return Stack(
+          children: [
+            // HPrimaryCard(),
+            Positioned(
+              child: Container(
+                width: 100.w,
+                height: 25.76.h,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(2, 97, 99, 1),
+                ),
+              ),
             ),
-          ),
-        ),
-        Positioned(left: 6.8.w, top: 17.41.h, child: HMTCard()),
-        Positioned(left: 2.7.w, right: 2.7.w, top: 7.6.h, child: HMTitle()),
-        Positioned(left: 20, right: 20, top: 212, child: JobListCard()),
-        Positioned(left: 50.5.w, top:17.41.h, child: HMTsmall()),
-      ],
+            Positioned(left: 6.8.w, top: 17.41.h, child: HMTCard()),
+            Positioned(left: 2.7.w, right: 2.7.w, top: 7.6.h, child: HMTitle()),
+            Positioned(left: 20, right: 20, top: 212, child: JobListCard()),
+            Positioned(left: 50.5.w, top:17.41.h, child: HMTsmall()),
+          ],
+        );
+      }
     );
   }
 }
